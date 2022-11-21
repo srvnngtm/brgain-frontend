2584964017f1ce5e5e78138a98f1cfdb7b8748aa
saran <sarav200898@gmail.com>
2022-11-21 16:03:39 -0500

    card layout, and list view

---
+++ lib/card_widget.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+import 'item.dart';
+
+class CardWidget extends StatelessWidget {
+
+  final Item item;
+  final Function() delete;
+
+  CardWidget( {required this.item, required this.delete } );
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Image(image: NetworkImage(item.imageLink
+                , scale: 2.5)),
+            SizedBox(height: 10,),
+            Text(item.name),
+            SizedBox(height: 10,),
+            TextButton.icon(
+                onPressed: delete,
+                icon: Icon(Icons.delete),
+                label: Text("delete"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/item.dart
@@ -0,0 +1,20 @@
+class Item {
+  String name;
+
+  String imageLink;
+
+  String productLink;
+
+  // Item({required String name,
+  //       required String imageLink,
+  //       required String productLink}) {
+  //
+  //   this.name = name;
+  //   this.imageLink = imageLink;
+  //   this.productLink = productLink;
+  // }
+
+  Item({required this.name, required this.imageLink, required this.productLink});
+
+
+}
