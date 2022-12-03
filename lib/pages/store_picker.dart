0011b7e21e9c099bc592e23333bc1830df964db5
saran <sarav200898@gmail.com>
2022-12-02 19:23:37 -0500

    store picker with multi picker

--- lib/pages/store_picker.dart
+++ lib/pages/store_picker.dart
@@ -1,41 +1,152 @@
 import 'package:brgain/models/store.dart';
 import 'package:flutter/material.dart';
-import 'package:multi_select_flutter/multi_select_flutter.dart';
 
 
 class StorePicker extends StatefulWidget {
-  const StorePicker({Key? key}) : super(key: key);
-
   @override
   State<StorePicker> createState() => _StorePickerState();
 }
 
 class _StorePickerState extends State<StorePicker> {
+  List<Store> itemList = [];
+  List<Store> selectedList = [] ;
 
+  @override
+  void initState() {
+    super.initState();
+    Map data =  ModalRoute.of(context)!.settings.arguments as Map;
+    // itemList = data['store'];
+    // selectedList = data['stores'];
 
-
+  }
   @override
   Widget build(BuildContext context) {
 
-    Map data = ModalRoute.of(context)!.settings.arguments as Map;
-    print(data);
-    List<Store> storeList = data['stores'];
+    // Map data = ModalRoute.of(context)!.settings.arguments as Map;
+    // print(data);
+    // itemList =  data['stores'];
+    // // List<Store> preSelected =  data['stores'];
+    // // selectedList = data['stores'];
+    List<int> preSelectedIds = [1, 2 , 3];
+    // selectedList = data['stores'];
+    // for (Store store in data['store']) {
+    //   preSelectedIds.add(store.storeId);
+    // }
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Stores"),
-        titleSpacing: 2.0,
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child:  Column(
-          children: storeList.map((e) => Text(e.storeName)).toList(),
-        ),
-      ),
+      appBar: getAppBar(),
+      body: GridView.builder(
+        itemCount: itemList.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 1.5,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10),
+        itemBuilder: (context, index) {
+          return GridItem(
+              item: itemList[index],
+              isSelected: (bool value) {
+                setState(() {
+                  if (value) {
+                    selectedList.add(itemList[index]);
+                  } else {
+                    selectedList.remove(itemList[index]);
+                  }
+                });
+                print("$index : $value");
+              },
+            isSel: preSelectedIds.contains(itemList[index].storeId),
+               );
+        }),
+
+    );
+  }
+
+
+  getAppBar() {
+    return AppBar(
+      title: Text(selectedList.length < 1
+          ? "Select Favourite Stores"
+          : "${selectedList.length} item selected"),
+      // actions: <Widget>[
+      //   selectedList.length < 1
+      //       ? Container()
+      //       : InkWell(
+      //       onTap: () {
+      //         setState(() {
+      //           for (int i = 0; i < selectedList.length; i++) {
+      //             itemList.remove(selectedList[i]);
+      //           }
+      //           selectedList = [];
+      //         });
+      //       },
+      //       child: const Padding(
+      //         padding: EdgeInsets.all(8.0),
+      //         child: Icon(Icons.delete),
+      //       ))
+      // ],
     );
   }
 
 
 
+}
+
+
+class GridItem extends StatefulWidget {
+  final Store item;
+  final ValueChanged<bool> isSelected ;
+  final bool isSel;
+
+  GridItem({required this.item, required this.isSelected, required this.isSel});
+
+  @override
+  _GridItemState createState() => _GridItemState();
+}
+
+class _GridItemState extends State<GridItem> {
+
+
+  bool isSelected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    isSelected = widget.isSel;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    // isSelected = widget.isSel;
+    return InkWell(
+      onTap: () {
+        setState(() {
+          isSelected = !isSelected;
+          widget.isSelected(isSelected);
+        });
+      },
+      child: Stack(
+        children: <Widget>[
+          Image.network(
+            widget.item.storeImageUrl,
+            color: Colors.black.withOpacity(isSelected ? 0.9 : 0),
+            colorBlendMode: BlendMode.color,
+          ),
+          isSelected
+              ? const Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.check_circle,
+                color: Colors.blue,
+              ),
+            ),
+          )
+              : Container()
+        ],
+      ),
+    );
+  }
 }
